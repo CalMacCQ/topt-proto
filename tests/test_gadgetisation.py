eb4ec0777a2e6c6edf0cd8449774cb02dafed3dc
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-08-26 14:25:45 +0100

    test gadgetisation separately

---
+++ tests/test_gadgetisation.py
@@ -0,0 +1,27 @@
+from pytket._tket.circuit import Circuit
+from pytket.passes import DecomposeBoxes, ComposePhasePolyBoxes
+from topt_proto.gadgetisation import REPLACE_HADAMARDS, get_n_internal_hadamards
+from topt_proto.utils import get_n_conditional_paulis
+
+
+def test_h_gadgetisation() -> None:
+    circ = (
+        Circuit(4)
+        .T(0)
+        .CX(0, 3)
+        .CX(2, 1)
+        .CX(3, 1)
+        .T(3)
+        .H(0)
+        .H(1)
+        .CZ(0, 3)
+        .H(2)
+        .CRy(0.25, 0, 3)
+    )
+    n_qubits_without_ancillas = circ.n_qubits
+    DecomposeBoxes().apply(circ)
+    ComposePhasePolyBoxes().apply(circ)
+    n_internal_h_gates = get_n_internal_hadamards(circ)
+    REPLACE_HADAMARDS.apply(circ)
+    assert get_n_conditional_paulis(circ) == n_internal_h_gates
+    assert circ.n_qubits == n_qubits_without_ancillas + n_internal_h_gates
--- tests/test_normal_form.py
+++ tests/test_normal_form.py
@@ -1,30 +1,7 @@
 from pytket.circuit import Circuit, OpType
-from pytket.passes import ComposePhasePolyBoxes, DecomposeBoxes
+from pytket.passes import ComposePhasePolyBoxes
 from topt_proto.gadgetisation import REPLACE_HADAMARDS, get_n_internal_hadamards
 from topt_proto.utils import get_n_conditional_paulis
-
-
-def test_h_gadgetisation() -> None:
-    circ = (
-        Circuit(4)
-        .T(0)
-        .CX(0, 3)
-        .CX(2, 1)
-        .CX(3, 1)
-        .T(3)
-        .H(0)
-        .H(1)
-        .CZ(0, 3)
-        .H(2)
-        .CRy(0.25, 0, 3)
-    )
-    n_qubits_without_ancillas = circ.n_qubits
-    DecomposeBoxes().apply(circ)
-    ComposePhasePolyBoxes().apply(circ)
-    n_internal_h_gates = get_n_internal_hadamards(circ)
-    REPLACE_HADAMARDS.apply(circ)
-    assert get_n_conditional_paulis(circ) == n_internal_h_gates
-    assert circ.n_qubits == n_qubits_without_ancillas + n_internal_h_gates
 
 
 def test_simple_circuit() -> None:
