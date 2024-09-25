7f0416ebb6c4a60ab373b32396ab02b0432579b2
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-09-25 17:23:17 +0100

    fix bad unitary test

--- tests/test_clifford.py
+++ tests/test_clifford.py
@@ -4,7 +4,9 @@ from pytket.qasm.qasm import circuit_from_qasm
 from pytket._tket.circuit import Circuit, PhasePolyBox
 from pytket.pauli import QubitPauliTensor
 from pytket.predicates import CliffordCircuitPredicate
+from pytket.passes import DecomposeBoxes
 from pytket.utils import compare_unitaries
+
 
 from topt_proto.clifford import synthesise_clifford, pauli_tensor_to_circuit
 from topt_proto.utils import tensor_from_x_index, REPLACE_T_WITH_RZ
@@ -13,21 +15,26 @@ from topt_proto.utils import tensor_from_x_index, REPLACE_T_WITH_RZ
 circuit_files = glob("qasm/*.qasm")
 
 
-def get_unitary_test_circuit(p_box: PhasePolyBox, pauli: QubitPauliTensor) -> Circuit:
+def get_unitary_test_circuit(
+    p_box: PhasePolyBox, pauli: QubitPauliTensor, decompose=False
+) -> Circuit:
     circ = Circuit(p_box.n_qubits)
     u_circ = p_box.get_circuit()
     u_dg_circ = u_circ.dagger()
-    circ.add_gate(PhasePolyBox(u_dg_circ), u_dg_circ.qubits)
+    circ.add_gate(PhasePolyBox(u_circ), u_circ.qubits)
     pauli_circ: Circuit = pauli_tensor_to_circuit(pauli)
     circ.append(pauli_circ)
-    circ.add_gate(PhasePolyBox(u_circ), u_circ.qubits)
+    circ.add_gate(PhasePolyBox(u_dg_circ), u_dg_circ.qubits)
+    # draw(circ)
+    if decompose:
+        DecomposeBoxes().apply(circ)
     return circ
 
 
 qpt = tensor_from_x_index(x_index=1, n_qubits=2)
 
-# test_cases = [(file, qpt) for file in circuit_files]
-test_cases = [("qasm/cnot_t_6.qasm", qpt)]
+test_cases = [(file, qpt) for file in circuit_files]
+# test_cases = [("qasm/cnot_t_6.qasm", qpt)]
 
 
 @pytest.mark.parametrize("qasm_file, pauli_op", test_cases)
@@ -38,7 +45,7 @@ def test_clifford_synthesis(qasm_file: str, pauli_op: QubitPauliTensor) -> None:
     clifford_circ = synthesise_clifford(pbox=phase_poly_box, input_pauli=pauli_op)
     assert CliffordCircuitPredicate().verify(clifford_circ)
     unitary_test_circuit = get_unitary_test_circuit(
-        p_box=phase_poly_box, pauli=pauli_op
+        p_box=phase_poly_box, pauli=pauli_op, decompose=True
     )
     test_unitary = unitary_test_circuit.get_unitary()
     result_unitary = clifford_circ.get_unitary()
