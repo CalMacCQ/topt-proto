94a8c12b46f7ae0bc5472fb01e2e5d8643381569
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-12-27 16:34:11 +0000

    add qft test

--- tests/test_gadgetisation.py
+++ tests/test_gadgetisation.py
@@ -1,5 +1,8 @@
+import pytest
+
 from pytket._tket.circuit import Circuit
 from pytket.passes import DecomposeBoxes, ComposePhasePolyBoxes
+
 from topt_proto.gadgetisation import REPLACE_HADAMARDS, get_n_internal_hadamards
 from topt_proto.utils import get_n_conditional_paulis
 
@@ -25,3 +28,29 @@ def test_h_gadgetisation() -> None:
     REPLACE_HADAMARDS.apply(circ)
     assert get_n_conditional_paulis(circ) == n_internal_h_gates
     assert circ.n_qubits == n_qubits_without_ancillas + n_internal_h_gates
+
+
+def build_qft_circuit(n_qubits: int) -> Circuit:
+    circ = Circuit(n_qubits, name="$$QFT$$")
+    for i in range(n_qubits):
+        circ.H(i)
+        for j in range(i + 1, n_qubits):
+            circ.CU1(1 / 2 ** (j - i), j, i)
+    for k in range(0, n_qubits // 2):
+        circ.SWAP(k, n_qubits - k - 1)
+    return circ
+
+
+n_qubit_cases = [2, 3, 7, 10]
+qft_circs = [build_qft_circuit(n) for n in n_qubit_cases]
+
+
+@pytest.mark.parametrize("n_qubits", n_qubit_cases)
+def test_gadgetisation_qft(n_qubits: int) -> None:
+    qft_circ: Circuit = build_qft_circuit(n_qubits)
+    ComposePhasePolyBoxes().apply(qft_circ)
+    n_internal_h_gates = get_n_internal_hadamards(qft_circ)
+    assert n_internal_h_gates == n_qubits - 1
+    REPLACE_HADAMARDS.apply(qft_circ)
+    assert get_n_conditional_paulis(qft_circ) == n_internal_h_gates
+    assert qft_circ.n_qubits == n_qubits + n_internal_h_gates
