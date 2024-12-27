d65af7ceb2b0d760333c6916b187d304dff4b6e1
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-12-27 18:49:17 +0000

    remove unused import

--- tests/test_gadgetisation.py
+++ tests/test_gadgetisation.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pytket._tket.circuit import Circuit, OpType
+from pytket._tket.circuit import Circuit
 from pytket.passes import DecomposeBoxes, ComposePhasePolyBoxes
 
 from topt_proto.gadgetisation import (
@@ -35,10 +35,10 @@ circ1 = Circuit(4).CCX(0, 1, 2).T(2).CX(2, 1).T(1).CCX(0, 1, 2)
 # External Hadamard before the boundary and eight internal Hadamards.
 circ2 = Circuit(4).CX(0, 3).T(3).H(0).T(2).H(1).CZ(0, 3).H(2).CRy(0.25, 0, 3)
 
-# Very simple test case. One internal Hadamard, zero external
+# Very simple test case. One internal Hadamard, zero external.
 circ3 = Circuit(2).CX(0, 1).T(1).CX(0, 1).H(0).CX(0, 1).T(1).CX(0, 1)
 
-# Two internal Hadamards after ComposePhasePolyBoxes, zero external
+# Two internal Hadamards after ComposePhasePolyBoxes, zero external.
 circ4 = Circuit(2).CX(0, 1).T(1).CX(0, 1).H(0).CX(0, 1).T(1).CX(0, 1)
 
 circuits = [circ0, circ1, circ2, circ3, circ4]
@@ -71,6 +71,8 @@ def build_qft_circuit(n_qubits: int) -> Circuit:
 n_qubit_cases = [2, 3, 7, 10]
 
 
+# The QFT has a regular structure, 1 external Hadamard
+# (After ComposePhasePolyBoxes) and (n-1) internal.
 @pytest.mark.parametrize("n_qubits", n_qubit_cases)
 def test_gadgetisation_qft(n_qubits: int) -> None:
     qft_circ: Circuit = build_qft_circuit(n_qubits)
